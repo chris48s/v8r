a875f80308cba6e911023399bc5bf4bf7f21f64f
chris48s <chris.shaw480@gmail.com>
2020-12-28 18:37:59 +0000

    add unit test for expire function

--- src/cache.spec.js
+++ src/cache.spec.js
@@ -4,7 +4,7 @@ const chai = require("chai");
 const assert = chai.assert;
 const flatCache = require("flat-cache");
 const nock = require("nock");
-const { cachedFetch } = require("./cache.js");
+const { cachedFetch, expire } = require("./cache.js");
 const { testCacheName, setUp, tearDown } = require("./test-helpers.js");
 
 describe("Cache", function () {
@@ -48,6 +48,34 @@ describe("Cache", function () {
       );
       assert.deepEqual(resp, { cached: false });
       mock.done();
+    });
+  });
+
+  describe("expire function", function () {
+    const messages = {};
+    const testCache = flatCache.load(testCacheName);
+    beforeEach(() => setUp(messages));
+    afterEach(() => tearDown());
+
+    it("Should delete expired and malformed cache objects", async function () {
+      const now = Date.now();
+      testCache.setKey("expired1", {
+        timestamp: now - 3001,
+        body: null,
+      });
+      testCache.setKey("expired2", {
+        timestamp: now - 20000,
+        body: null,
+      });
+      testCache.setKey("fresh", {
+        timestamp: now + 5000,
+        body: null,
+      });
+      testCache.setKey("malformed", {
+        timestamp: now + 5000,
+      });
+      expire(testCache, 3000);
+      assert.deepEqual(Object.keys(testCache.all()), ["fresh"]);
     });
   });
 });
