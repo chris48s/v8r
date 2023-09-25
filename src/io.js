3b16f13d262f09b0735a5bc1539f6d74ca2657eb
Abrifq <melezorus34@gmail.com>
2023-09-25 21:53:23 +0300

    Bodged together a yaml JSON-Schema support

--- src/io.js
+++ src/io.js
@@ -1,18 +1,27 @@
 import fs from "fs";
 import path from "path";
 import isUrl from "is-url";
+import { parseFile } from "./parser";
 
 async function getFromUrlOrFile(location, cache, base = null) {
   if (isUrl(location)) {
     return await cache.fetch(location);
   } else {
     if (base != null) {
-      return JSON.parse(
-        await fs.promises.readFile(path.join(base, location), "utf8"),
-      );
+      if (location.endsWith(".yml") || location.endsWith(".yaml"))
+        return parseFile(
+          await fs.promises.readFile(path.join(base, location), "utf8"),
+          ".yaml",
+        );
+      else
+        return JSON.parse(
+          await fs.promises.readFile(path.join(base, location), "utf8"),
+        );
     }
   }
-  return JSON.parse(await fs.promises.readFile(location, "utf8"));
+  if (location.endsWith(".yml") || location.endsWith(".yaml"))
+    return parseFile(await fs.promises.readFile(location, "utf8"), ".yaml");
+  else return JSON.parse(await fs.promises.readFile(location, "utf8"));
 }
 
 export { getFromUrlOrFile };
