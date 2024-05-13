e492b7374926914ff2785086de4405713e7a6ec6
chris48s <git@chris-shaw.dev>
2024-05-13 10:48:57 +0100

    switch to flat config

---
+++ eslint.config.cjs
@@ -0,0 +1,30 @@
+const globals = require("globals");
+const js = require("@eslint/js");
+const prettierConfig = require("eslint-config-prettier");
+const prettierPlugin = require("eslint-plugin-prettier");
+const mochaPlugin = require("eslint-plugin-mocha");
+
+module.exports = [
+  js.configs.recommended,
+  mochaPlugin.configs.flat.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      mocha: mochaPlugin,
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        mocha: true,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "prettier/prettier": ["error"],
+      "mocha/no-skipped-tests": ["error"],
+      "mocha/no-exclusive-tests": ["error"],
+    },
+  },
+];
